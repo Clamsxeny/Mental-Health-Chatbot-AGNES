5edd2d7b22903a232d22735fc0e6a8e33fa3418a
Claxy <88501209+Clamsxeny@users.noreply.github.com>
2023-12-14 11:38:13 +0700

    Delete Time.py

--- Time.py
+++
@@ -1,7 +0,0 @@
-from datetime import datetime
-
-def gettime():
-    current_time = datetime.now()
-    formatted_date = current_time.strftime("%d/%m/%Y")
-    formatted_time = current_time.strftime("%H:%M:%S")
-    return f"Day today: {formatted_date}\nCurrent time: {formatted_time}"
